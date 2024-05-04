e5af4463ae5c1a1e4dd4ceb52c908a0c2541a7b3
Vikas bisariya <101662974+Vikasbisariya-dev@users.noreply.github.com>
2024-05-04 15:27:37 +0530

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -14,12 +14,6 @@ import UserList from "./components/pages/UserList";
 // import MenuForm from "./components/pages/MenuForm";
 import MenuList from "./components/pages/MenuList";
 import RoleList from "./components/pages/RoleList";
-import AddDocDetailList from "./components/pages/AddDocDetailList";
-import MedFreqList from "./components/pages/MedFreqList";
-import AilTimingList from "./components/pages/AilmentTimingList";
-import MedTimingList from "./components/pages/MedTimingList";
-import AilmentList from "./components/pages/AilmentList";
-import RefferalList from "./components/pages/RefferalList";
 
 const router = createBrowserRouter([
   {
@@ -32,7 +26,7 @@ const router = createBrowserRouter([
     element: <PersistLogin />,
     children: [
       {
-        // element: <ProtectedRoute />,
+        element: <ProtectedRoute />,
         children: [
           {
             path: "/ohcSelection",
@@ -56,7 +50,7 @@ const router = createBrowserRouter([
         element: <PersistLogin />,
         children: [
           {
-            // element: <ProtectedRoute />,
+            element: <ProtectedRoute />,
             children: [
               {
                 path:'/adminHome',
@@ -65,18 +59,6 @@ const router = createBrowserRouter([
               {
                 path:'/ohcList',
                 element:<OhcList />
-              },
-              {
-                path:'/medFreqList',
-                element:<MedFreqList />
-              },
-              {
-                path:'/medTimingList',
-                element:<MedTimingList />
-              },
-              {
-                path:'/AilTimingList',
-                element:<AilTimingList/>
               },
               {
                 path:'/userList',
@@ -89,18 +71,6 @@ const router = createBrowserRouter([
               {
                 path:'/menuList',
                 element:<MenuList />
-              },
-              {
-                path:'/addDocdetailList',
-                element:<AddDocDetailList/>
-              },
-              {
-                path:'/ailmentList',
-                element:<AilmentList/>
-              },
-              {
-                path:'/refferalList',
-                element:<RefferalList/>
               }
             ],
           },
