b0dd4500acb8770208ef85a2f6c4337401837772
Devendra Yadav <211b107@juetguna.in>
2024-05-21 19:35:33 +0530

    Plant, Unit, Vaccine Forms

---
+++ src/components/pages/PlantForm.jsx
@@ -0,0 +1,67 @@
+import Ohclogo from "./Ohclogo";
+import { FormControl, Grid} from "@mui/material";
+import PropTypes from "prop-types";
+import Input from "../common/Input";
+import { InputLabel, MenuItem, Select } from "@mui/material";
+
+
+
+const PlantForm = ({
+  values,
+  touched,
+  handleBlur,
+  errors,
+  handleChange,
+  setFieldValue,
+  handleSubmit,
+}) => {
+    PlantForm.propTypes = {
+    values: PropTypes.object.isRequired,
+    touched: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired,
+    handleBlur: PropTypes.func.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    setFieldValue: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+  };
+
+
+//   const Medselect = ["Active", "Not Active"];
+//   const isDefault = ["Yes", "No"];
+
+
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2} justifyContent="center" alignItems="center" sx={{width:400}} >
+          <Grid item xs={12}  justifyContent="center" alignItems="center">
+            <FormControl fullWidth>
+              <Grid container spacing={2} justifyContent="center" alignItems="center">
+                <Grid item xs={12}  container spacing={1} justifyContent="center" alignItems="center">
+                  <Input
+                    label="Plant Name"
+                    name="PlantName"
+                    type="text"
+                    size="large"
+                    value={values.PlantName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={
+                      errors.PlantName && touched.PlantName? (
+                        <span style={{ color: "red" }}>{errors.PlantName}</span>
+                      ) : null
+                    }
+                  />
+                </Grid>
+              </Grid>
+            </FormControl>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+};
+
+
+export default PlantForm;
