f2cf4c9b7c7e9889412a033f1a8b0b3fec377791
Sachin <sachking51@gmail.com>
2024-06-06 21:17:17 +0530

    SlotsList & BodyMedicalWasteM Form&List

---
+++ src/components/pages/SlotsListForm.jsx
@@ -0,0 +1,126 @@
+import { FormControl, Grid } from "@mui/material";
+import PropTypes from "prop-types";
+import Input from "../common/Input";
+import SingleSelect from "../common/SingleSelect";
+
+
+//import TextArea from "../TextArea";
+
+const SlotsListForm = ({
+  values,
+  touched,
+  handleBlur,
+  errors,
+  handleChange,
+  // setFieldValue,
+  handleSubmit,
+}) => {
+    SlotsListForm.propTypes = {
+    values: PropTypes.object.isRequired,
+    touched: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired,
+    handleBlur: PropTypes.func.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    setFieldValue: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+  };
+
+
+ const record=["##","## "]
+ const CaseType =["#","#","#"]
+ 
+
+ 
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+    <form onSubmit={handleSubmit}>
+    <Grid container spacing={2} justifyContent="center" alignItems="center" sx={{width:360}}>
+       
+       <Grid item xs={12}  justifyContent="center" alignItems="center">
+        
+         <FormControl fullWidth>
+           <Grid container spacing={2} justifyContent="center" alignItems="center">
+           <Grid item xs={12}  container spacing={1} justifyContent="center" alignItems="center"> 
+
+           <Input label=""
+                    name="date"
+                    type="date"
+                    size="large"
+                    value={values.date}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={
+                      errors.date && touched.date? (
+                        <span style={{ color: "red" }}>{errors.date}</span>
+                      ) : null
+                    }
+                  />
+                </Grid>
+             <Grid item xs={12}  container spacing={1} justifyContent="center" alignItems="center"> <SingleSelect
+                    arr={record}
+                    label="Select Doctor Name"
+                    name="name"
+                    value={values.name}
+                    onChange={(event, newValue) => {
+                      const syntheticEvent = {
+                        target: {
+                          name: "name",
+                          value: newValue,
+                        },
+                      };
+                      handleChange(syntheticEvent);
+                    }}
+                    onBlur={handleBlur}
+                    type="text"
+                    helperText={
+                      errors.name && touched.name ? (
+                        <span style={{ color: "red" }}>{errors.name}</span>
+                      ) : null
+                    }
+                  />
+                </Grid>
+          
+             
+                
+              
+                <Grid item xs={12}  container spacing={1} justifyContent="center" alignItems="center"> 
+                <SingleSelect
+                
+                    arr={CaseType}
+                    label="Select Case Type"
+                    name="type"
+                    value={values.type}
+                    onChange={(event, newValue) => {
+                      const syntheticEvent = {
+                        target: {
+                          name: "type",
+                          value: newValue,
+                        },
+                      };
+                      handleChange(syntheticEvent);
+                    }}
+                    // onChange={handleChange}
+                    onBlur={handleBlur}
+                    type="text"
+                    helperText={
+                      errors.type && touched.type ? (
+                        <span style={{ color: "red" }}>
+                          {errors.type}
+                        </span>
+                      ) : null
+                    }
+                  />
+                </Grid>
+               
+                            </Grid>
+            </FormControl>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+};
+
+export default SlotsListForm;
+
+  
